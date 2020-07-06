3269e1e7e20df76e7b1a099799c76ea160fb1798
Aleksei Chikin <mail@makingoff.name>
2020-07-06 22:46:08 +0300

    Initial commit

---
+++ gutt-asset.js
@@ -0,0 +1,21 @@
+const { Asset } = require('parcel-bundler')
+const gutt = require('gutt')
+const stringifier = require('gutt-browser-render-stringifier')
+
+class GuttAsset extends Asset {
+	constructor(name, options) {
+		super(name, options)
+
+		this.type = 'js'
+	}
+
+	async parse(code) {
+		return gutt.parse(code)
+	}
+
+	async generate() {
+		return stringifier(this.ast)
+	}
+}
+
+module.exports = GuttAsset
